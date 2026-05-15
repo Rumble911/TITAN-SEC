dac8908d71ce1ea015ac1e81e6913eab0fbf9515
عبد الله ابراهيم حسن ابراهيم <abdallahalqam4040@gmail.com>
2026-05-15 21:03:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20006,7 +20006,7 @@ def osint_intelbase_email_route():
         }), 400
     
     # IntelBase API Configuration
-    INTELBASE_API_KEY = os.environ.get('INTELBASE_API_KEY', 'in_kmnBuC89MvFYkWSbGqQu')
+    INTELBASE_API_KEY = os.environ.get('INTELBASE_API_KEY', 'in_aNYXYXmtzcWZbict8wji')
     intelbase_url = 'https://api.intelbase.is/lookup/email'
     
     payload = {
